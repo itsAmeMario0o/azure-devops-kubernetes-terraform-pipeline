445f83eade93cb168e757a2aa95c21ee30d03b25
mariorui <mariorui@mariorui-m-44f5.lan>
2022-03-28 14:18:04 -0400

    Revised AWS main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,7 +1,7 @@
 # aws --version
 # aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
+# terraform-backend-state
 # AKIA4AHVNOD7OOO6T4KI
 
 
